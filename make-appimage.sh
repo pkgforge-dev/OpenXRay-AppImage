5c3ed6203e72f28b0b779deb6022e60c552d005a
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:17:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -20,7 +20,3 @@ echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire*.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --simple-test ./dist/*.AppImage
